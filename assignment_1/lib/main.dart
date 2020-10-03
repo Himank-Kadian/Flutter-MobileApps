803f8c8dc4ae767bbe656d37657faa202eb1a387
Himank-Kadian <60160747+Himank-Kadian@users.noreply.github.com>
2020-10-02 21:20:56 -0600

    Created column 1 containers.

--- assignment_1/lib/main.dart
+++ assignment_1/lib/main.dart
@@ -2,42 +2,76 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text("Row and Column Widgets") ,
+          title: Text("Row and Column Widgets"),
         ),
-        body: Row(children: [
-          Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(10.0),
-                height: 100.0,
-                width: 100.0,
-                child: Text(
-                  'Container 1'
-                ),
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Colors.orange,
-                )
+        body: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 0, 10, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                      padding: EdgeInsets.all(10.0),
+                      height: 100.0,
+                      width: 100.0,
+                      child: Center(
+                        child: Text('Container 1'),
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 3),
+                        shape: BoxShape.rectangle,
+                        color: Colors.amber,
+                      )),
+                  Container(
+                      padding: EdgeInsets.all(10.0),
+                      height: 100.0,
+                      width: 100.0,
+                      transform: Matrix4.rotationZ((22 / 7) / 4),
+                      child: Center(
+                        child: Text('Container 2'),
+                      ),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        color: Colors.white,
+                      )),
+                ],
               ),
-              Container(
-
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 0, 10, 0),
+              child: Column(
+                children: [
+                  Container(
+                    
+                  ),
+                  Container(
+                    
+                  ),
+                ],
               ),
-            ],
-          ),
-          Column(
-
-          ),
-          Column(
-
-          ),
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Container(
+                      ),
+                  Container(
+                    
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
